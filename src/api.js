f8b4d419241c4ff7b94c2933642df9a9cca4d007
Ivan Krisko <vanya9.9@mail.ru>
2021-05-03 02:04:34 +1000

    added function 'getCoinList' that load available currencies

--- src/api.js
+++ src/api.js
@@ -5,3 +5,9 @@ export function loadCurrencies(currencies){
     return fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${currencies.join(',')}&tsyms=USD&api_key=${API_KEY}`)
           .then((responce)=>responce.json())
 }
+
+export async function getCoinList(){
+    return fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true')
+        .then((responce)=>responce.json())
+        .then((responce)=>responce.Data)
+}
