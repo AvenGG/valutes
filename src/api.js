073524a8e2e553e302140b1ee7bcc1ad8649b3b7
Ivan Krisko <vanya9.9@mail.ru>
2021-05-16 21:49:43 +1000

    added function that load single price pair

--- src/api.js
+++ src/api.js
@@ -7,7 +7,20 @@ export function loadCurrencies(currencies){
 }
 
 export async function getCoinList(){
-    return fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true')
+   let coinList = await fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true')
         .then((responce)=>responce.json())
         .then((responce)=>responce.Data)
+    let result ={}
+        for (let coin of Object.keys(coinList)){
+              result[coin.toUpperCase()] = {
+                name: coinList[coin]['FullName'].split('(')[0],
+                code: coin
+              }
+            }
+    return result
+}
+
+export function loadPair(a,b){
+  return fetch(`https://min-api.cryptocompare.com/data/price?fsym=${a}&tsyms=${b}&api_key=${API_KEY}`)
+          .then((responce)=>responce.json())
 }
