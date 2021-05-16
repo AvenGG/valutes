36a74104264cb51fd663684e2c6055902c05e22f
Ivan Krisko <vanya9.9@mail.ru>
2021-05-17 01:12:03 +1000

    added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,69 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Authorization from '../views/Authorization.vue'
+import Registration from '../views/Registration.vue'
+import NavigationPanel from '../views/NavigationPanel.vue'
+import Wallet from '../views/Wallet.vue'
+import NotFound from '../views/404.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: NavigationPanel,
+    children:[
+      {
+        path: '',
+        name: 'Home',
+        component: Home
+      },
+      {
+        path: 'wallet',
+        name: "Wallet",
+        component: Wallet,
+        props: true,
+        beforeEnter: (to, from, next) =>{
+          if(localStorage.getItem('user'))
+            next()
+          else{
+            next({name: 'Authorization'})
+          }
+        }
+      },
+      {
+        path: 'authorization',
+        name: 'Authorization',
+        component: Authorization,
+        beforeEnter: (to, from, next) =>{
+          if(!localStorage.getItem('user'))
+            next()
+          else{
+            next({name: 'Home'})
+          }
+        }
+      },
+      {
+        path: 'registration',
+        name: 'Registration',
+        component: Registration,
+        beforeEnter: (to, from, next) =>{
+          if(!localStorage.getItem('user'))
+            next()
+          else{
+            next({name: 'Home'})
+          }
+        }
+      },
+
+    ]
   },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: NotFound
+  }
  
 ]
 
